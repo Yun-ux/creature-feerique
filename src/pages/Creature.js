b3f18bd677038fb9785514c9b96505e2b8843c97
Yuna <hinamoriyasmina@gmail.com>
2020-01-23 13:54:51 +0100

    bug in creature.js

--- src/pages/Creature.js
+++ src/pages/Creature.js
@@ -1,12 +1,26 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
+const CreatureDetail = ({ id }) => {
+  const [creature, setCreature] = useState(null);
 
-const CreatureDetail = ({id}) => {
-
-
-return (
-<div>{id}</div>);
+  useEffect(() => {
+    fetch(`/data/${id}.json`)
+      .then(r => r.json())
+      .then(data => {
+        setCreatures(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>{creature && creature.nom}</h1>
+      <p>{creature && creature.description}</p>
+      <ol>
+        {creature?.pouvoir?.map(p => (
+          <li>{p}</li>
+        ))}
+      </ol>
+    </div>
+  );
 };
-
 
 export default CreatureDetail;
